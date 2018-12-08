07fe42be70061cb86a48014129565611796d034f
NoelleDL <noelle.legrain@gmail.com>
2018-12-08 23:11:36 +0000

    fix error in code so that chores array empties and then repopulates with chores

--- spec/choresSpec.js
+++ spec/choresSpec.js
@@ -1,6 +1,6 @@
 describe("Chores", function() {
   it("returns a random chore", function() {
-    result = Chores();
+    result = getChores(chores, completedChores);
     expect(result.length).toEqual(1);
   });
 });
--- src/chores.js
+++ src/chores.js
@@ -1,17 +1,30 @@
-function Chores() {
-  const chores = [
-      "cleaning the bathroom sink - upstairs",
-      "cleaning the bathroom sink - downstairs",
-      "dusting the family room",
-      "sweeping the kithchen floor",
-    ];
+var chores = [
+    "Dust the family room",
+    "Sweep the kithchen floor",
+    "Clean bathroom",
+  ];
 
-  const chore = [chores[Math.floor(Math.random() * chores.length)]];
+var completedChores = [];
+var chore = "";
+
+function getChores(chores, completedChores) {
+
+  if (this.chores.length === 0) {
+    this.chores = this.completedChores;
+    this.completedChores = [];
+  }
+
+  chore = this.chores[Math.floor(Math.random()*chores.length)];
+
+  completedChores.push(chore);
+  var deleteChore = chores.indexOf(chore);
+  chores.splice(deleteChore, 1);
 
   $('#exampleModalCenter').on('show.bs.modal', function (event) {
     var modal = $(this);
     modal.find('.modal-title').text("Today, you get to:");
     modal.find('.modal-body').text(`${chore}`);
   });
+
   return chore;
 }
